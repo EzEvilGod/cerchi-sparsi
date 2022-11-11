347ad4ffed68ef130cf4181a7317d1477efdbbf2
EzEvilGod <marco.ferri2005@gmail.com>
2022-11-11 19:01:34 +0100

    creazioni dei cerchi

--- static/js/libs/p5js/sketch.js
+++ static/js/libs/p5js/sketch.js
@@ -9,4 +9,23 @@ function setup()
     background("pink")
 }
 
+function draw()
+{
+    dati(x,y,d,a,color1,color2,color3)
+    circle(x,y,d)
+    strokeWeight(1)
+    fill(color1, color2, color3, a)
 
+}
+
+function dati()
+{
+    x = random(0,w)
+    y = random(0,h)
+    d = random(30,90)
+
+    color1 = random(0,255)
+    color2 = random(0,255)
+    color3 = random(0,255)
+    a = random(0,255)
+}
